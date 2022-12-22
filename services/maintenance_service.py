302f81961e58b18f326049ae38316d7610bf07e2
Exide <ig.roman95@mail.ru>
2022-12-22 03:18:18 +0300

    Added more maintenance commands

--- services/maintenance_service.py
+++ services/maintenance_service.py
@@ -10,8 +10,10 @@ class MaintenanceCommand:
 class MaintenanceService:
     __commands = [
         MaintenanceCommand('Git Pull', 'Admin panel', 'git pull'),
+        MaintenanceCommand('Restart API', 'Admin panel', 'sudo service admin_panel restart'),
+        MaintenanceCommand('Re-deploy web', 'Admin panel', 'npm run build --prefix ~/repos/admin_panel/client-app && cp -r ~/repos/admin_panel/client-app/build/* /usr/share/nginx/www/admin.exideprod.com'),
         MaintenanceCommand('Restart hub', 'Bot Exide', 'sudo service bot_exide_hub restart'),
-        MaintenanceCommand('Re-deploy hub web', 'Bot Exide', 'npm run build --prefix client-app && cp -r client-app/build/* /usr/share/nginx/www/bot.exideprod.com'),
+        MaintenanceCommand('Re-deploy web', 'Bot Exide', 'npm run build --prefix ~/repos/bot_exide/hub/client-app && cp -r ~/repos/bot_exide/hub/client-app/build/* /usr/share/nginx/www/bot.exideprod.com'),
         MaintenanceCommand('Restart Discord Bot', 'Bot Exide', 'sudo service discord_bot restart'),
         MaintenanceCommand('Restart Telegram Bot', 'Bot Exide', 'sudo service telegram_bot restart'),
         MaintenanceCommand('Restart VK Bot', 'Bot Exide', 'sudo service vk_bot restart'),
