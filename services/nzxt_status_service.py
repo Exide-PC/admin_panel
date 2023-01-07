75e0d15066ec55770214e0b29b54b355f3faf590
Exide <ig.roman95@mail.ru>
2023-01-07 17:41:02 +0300

    Used "sensors" package for cpu temperature.

--- services/nzxt_status_service.py
+++ services/nzxt_status_service.py
@@ -1,14 +1,22 @@
+import re
 import subprocess
 
 class NzxtStatus:
     def __init__(self, cpu_temperature: int) -> None:
         self.cpu_temperature = cpu_temperature
 
+# https://phoenixnap.com/kb/linux-cpu-temp
+
 class NzxtStatusService:
     def __init__(self) -> None:
         pass
 
     def get_status(self):
-        result = subprocess.run(['cat', '/sys/class/thermal/thermal_zone1/temp'], stdout=subprocess.PIPE)
-        cpu_temperature = int(result.stdout.decode('utf-8')) / 1000
-        return NzxtStatus(cpu_temperature)
+        result = subprocess.run(['sensors'], stdout=subprocess.PIPE)
+        lines = result.stdout.decode('utf-8').split('\n')
+
+        cpu_line = next(l for l in lines if l.startswith('Package id 0'))
+        matches = re.findall(r'(\d+\.\d+)°C', cpu_line)
+        numbers = list(map(lambda m: int(m), matches))
+
+        return NzxtStatus(numbers[0])
