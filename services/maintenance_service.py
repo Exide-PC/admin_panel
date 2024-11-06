a61fb363835e036cec23970e272b60ddfaca821c
Exide <ig.roman95@mail.ru>
2024-11-07 00:13:51 +0300

    Added commands to restart nginx.

--- services/maintenance_service.py
+++ services/maintenance_service.py
@@ -20,6 +20,7 @@ class MaintenanceService:
         MaintenanceCommand('91d16268-7649-450e-bc8b-0ed8a0a63b86', 'Compose restart', 'Bot Exide [Prod]', 'docker compose -f /home/exide/repos/bot_exide_bare/production/compose.yaml --env-file /home/exide/repos/bot_exide_bare/production/.env.docker.prod restart'),
         MaintenanceCommand('dc45bbd3-1676-48da-9777-448232de7d41', 'Compose down', 'Bot Exide [Prod]', 'docker compose -f /home/exide/repos/bot_exide_bare/production/compose.yaml --env-file /home/exide/repos/bot_exide_bare/production/.env.docker.prod down'),
         MaintenanceCommand('81f3e03e-6b6e-4e9c-b5ef-c858f8991e38', 'Restart bot manager', 'Bot Exide [Prod]', 'docker compose -f /home/exide/repos/bot_exide_bare/production/compose.yaml --env-file /home/exide/repos/bot_exide_bare/production/.env.docker.prod restart telegram_bot_manager'),
+        MaintenanceCommand('0b1b5fe0-7f06-4327-864c-9025c8f36570', 'Restart proxy', 'Bot Exide [Prod]', 'docker compose -f /home/exide/repos/bot_exide_bare/production/compose.yaml --env-file /home/exide/repos/bot_exide_bare/production/.env.docker.prod restart nginx'),
 
         MaintenanceCommand('a5e852ae-a1fd-450f-b15f-6c59226dffc7', 'Git Pull', 'Bot Exide [Staging]', 'git -C ~/repos/bot_exide_bare/staging pull'),
         MaintenanceCommand('c5241166-f30d-41da-b782-0ac75837a3b1', 'Compose build', 'Bot Exide [Staging]', 'docker compose -f /home/exide/repos/bot_exide_bare/staging/compose.yaml --env-file /home/exide/repos/bot_exide_bare/staging/.env.docker.staging up --build -d'),
@@ -27,6 +28,7 @@ class MaintenanceService:
         MaintenanceCommand('aaf776e2-127d-4c95-9fce-5978572a4ac6', 'Compose restart', 'Bot Exide [Staging]', 'docker compose -f /home/exide/repos/bot_exide_bare/staging/compose.yaml --env-file /home/exide/repos/bot_exide_bare/staging/.env.docker.staging restart'),
         MaintenanceCommand('83012220-828d-47f0-92b5-5a5914235323', 'Compose down', 'Bot Exide [Staging]', 'docker compose -f /home/exide/repos/bot_exide_bare/staging/compose.yaml --env-file /home/exide/repos/bot_exide_bare/staging/.env.docker.staging down'),
         MaintenanceCommand('d927a3ae-dd5e-4079-9a3f-405a7b28131e', 'Restart bot manager', 'Bot Exide [Staging]', 'docker compose -f /home/exide/repos/bot_exide_bare/staging/compose.yaml --env-file /home/exide/repos/bot_exide_bare/staging/.env.docker.staging restart telegram_bot_manager'),
+        MaintenanceCommand('14ff2a66-388f-411f-81af-09e6f457f2e4', 'Restart proxy', 'Bot Exide [Staging]', 'docker compose -f /home/exide/repos/bot_exide_bare/staging/compose.yaml --env-file /home/exide/repos/bot_exide_bare/staging/.env.docker.staging restart nginx'),
     ]
     
     def __init__(self) -> None:
